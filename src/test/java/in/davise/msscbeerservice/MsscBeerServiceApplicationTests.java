ea91c46cb5d81e3450cfcb1967ac92490d0ea3a3
Sethuraman Srinivasan <sethu10121994@gmail.com>
2020-05-16 19:48:19 +0530

    :tada: Initial Commit

---
+++ src/test/java/in/davise/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.davise.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
